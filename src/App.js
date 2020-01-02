800132fa18269875cf368ec4c48347729ea1a228
CScori <49799016+CScori@users.noreply.github.com>
2020-01-02 09:37:06 -0800

    adds header nav with links waiting for marketing for 100

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 
 //component import for sign up and login
-
+import Private from "./Components/Routes/Private"
 function App() {
   return (
    <Router>
---
+++ src/Components/Header/header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+
+export const header = () => {
+    return (
+        <div className='header'>
+            <span className='logo'>
+                MockEnterView
+            </span>
+            <nav>
+                <span className='link'>
+                    <a>Home</a>
+                {/* need links to deployed pages */}
+                    <a>About</a>
+                {/* Need links to about page */}
+                    <a>For Employees</a>
+                </span>
+                <span className='auth'>
+                    <Link to='/login'>Login</Link>
+                    <Link to='/'>Sign Up</Link>
+                </span>
+            </nav>
+        </div>
+    )
+}
