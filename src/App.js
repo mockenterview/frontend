01efa8bbbdcb2811093ebb32f09a0c3e6a7f79ca
CScori <49799016+CScori@users.noreply.github.com>
2020-01-02 12:23:08 -0800

    adds full design of headers not functional

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
-import Register from './Components/Register/Register'
+
 
 //component import for sign up and login
 import Private from "./Components/Routes/Private"
@@ -9,6 +9,7 @@ import Private from "./Components/Routes/Private"
 // style imports
 import Header from "./Components/Header/home-header"
 import SideBar from "./Components/Header/sideBar"
+
 
 function App() {
   return (
