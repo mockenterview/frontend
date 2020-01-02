7dee4ef22925683c9f6792a0b7a7bed151d93c2c
Derek Etman <dereketman@gmail.com>
2020-01-02 13:08:50 -0800

    + Simple Layout Login & Reg

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
-import Register from './Components/Register/Register'
 
 //component import for sign up and login
 import Private from "./Components/Routes/Private"
+import {Login} from "./Components/Login/Login"
+import {Register} from "./Components/Register/Register"
 // style imports
 import Header from "./Components/Header/home-header"
 
@@ -16,8 +17,8 @@ function App() {
      <div className='App'>
        <Header />
        
-       {/* <Route exact path='/' component ={SignUp} />
-       <Route exact path='/login' component ={Login} /> */}
+       <Route exact path='/' component ={Register} />
+       <Route exact path='/login' component ={Login} />
      </div>
    </Router>
   );
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,108 +1,131 @@
 import React from "react";
-import { Formik, Field, Form, useFormik, ErrorMessage, withFormik, useField } from "formik";
+import {
+	Formik,
+	Field,
+	Form,
+	useFormik,
+	ErrorMessage,
+	withFormik,
+	useField
+} from "formik";
 import * as Yup from "yup";
-import { Container, Row, Col, Label } from 'reactstrap';
-import axios from 'axios';
-import {Link} from 'react-router-dom';
+import { Container, Row, Button, Alert } from "reactstrap";
 
-const MyCheckbox = ({ children, ...props }) => {
-  const [field, meta] = useField({ ...props, type: 'checkbox' });
-  return (
-    <>
-      <label className="checkbox">
-        <input type="checkbox" {...field} {...props} />
-        {children}
-      </label>
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
-    </>
-  );
+export const MyCheckbox = ({ children, ...props }) => {
+	const [field, meta] = useField({ ...props, type: "checkbox" });
+	return (
+		<>
+			<label className="checkbox">
+				<input type="checkbox" {...field} {...props} />
+				{children}
+			</label>
+			{meta.touched && meta.error ? (
+				<div className="error">{meta.error}</div>
+			) : null}
+		</>
+	);
 };
 
-const MyTextInput = ({ label, ...props }) => {
-  // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
-  // spread into <input> and also replace ErrorMessage entirely.
-  const [field, meta] = useField(props);
-  return (
-    <>
-      <label htmlFor={props.id || props.name}>{label}</label>
-      <input className="text-input" {...field} {...props} />
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
-    </>
-  );
+export const MyTextInput = ({ label, ...props }) => {
+	// useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
+	// spread into <input> and also replace ErrorMessage entirely.
+	const [field, meta] = useField(props);
+	return (
+		<>
+			<label htmlFor={props.id || props.name}>{label}</label>
+			<input className="text-input" {...field} {...props} />
+			{meta.touched && meta.error ? (
+				<div className="error">{meta.error}</div>
+			) : null}
+		</>
+	);
 };
 
+export const Register = ({ values, errors, touched, status }) => {
+	return (
+		<Container fluid="sm" className="register_wrapper">
+			<Row>
+				<h3>Sign Up</h3>
+			</Row>
+			<Row>
+				<a href="/login">Already have an account?</a>
+			</Row>
+			<Formik
+				initialValues={{ firstName: "", lastName: "", email: "" }}
+				validationSchema={Yup.object({
+					firstName: Yup.string()
+						.max(15, "Must be 15 characters or less")
+						.required(<Alert color="warning">First Name Required!</Alert>),
+					lastName: Yup.string()
+						.max(20, "Must be 20 characters or less")
+						.required(<Alert color="warning">Last Name Required!</Alert>),
+					email: Yup.string()
+						.email("Invalid email address")
+						.required(<Alert color="warning">Email Required!</Alert>),
+					password: Yup.string()
+						.min(8, "Must be more than 8 characters")
+						.required(<Alert color="warning">Password Required!</Alert>),
+					acceptedTerms: Yup.boolean()
+						.required("Required")
+						.oneOf([true], "You must accept the terms and conditions to continue")
+				})}
+				onSubmit={(values, { setSubmitting }) => {
+					setTimeout(() => {
+						alert(JSON.stringify(values, null, 2));
+						setSubmitting(false);
+					}, 400);
+				}}
+			>
+				<Form>
+					<Row>
+						<MyTextInput
+							name="firstName"
+							type="text"
+							placeholder="First Name"
+						/>
+					</Row>
 
+					<Row>
+						<MyTextInput
+							name="lastName"
+							type="text"
+							placeholder="Last Name"
+						/>
+					</Row>
 
-const Register = ({ values, errors, touched, status }) => {
+					<Row>
+						<MyTextInput
+							name="email"
+							type="email"
+							placeholder="Email"
+						/>
+					</Row>
 
+					<Row>
+						<MyTextInput name="password" placeholder="Create Password"/>
+					</Row>
 
-  return (
-    <Formik
-      initialValues={{ firstName: '', lastName: '', email: '' }}
-      validationSchema={Yup.object({
-        firstName: Yup.string()
-          .max(15, 'Must be 15 characters or less')
-          .required('Required'),
-        lastName: Yup.string()
-          .max(20, 'Must be 20 characters or less')
-          .required('Required'),
-        email: Yup.string()
-          .email('Invalid email address')
-          .required('Required'),
-        password: Yup.string()
-        .min(8, 'Must be more than 8 characters')
-        .required('Required'),
-        acceptedTerms: Yup.boolean()
-        .required('Required')
-        .oneOf([true], 'You must accept the terms and conditions to continue')
-      })}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
-      }}
-    >
-      <Form>
+					<Row>
+						<MyTextInput name="confirmPassword" placeholder="Confirm Password" />
+					</Row>
 
-        <MyTextInput 
-        label="First Name"
-        name="firstName"
-        type="text"
-        placeholder="First Name"
-        />
+					<Row>
+						<MyCheckbox name="acceptedTerms">
+							I accept the terms and conditions
+						</MyCheckbox>
+					</Row>
 
-        <MyTextInput
-        label="Last Name"
-        name="lastName"
-        type="text"
-        placeholder="Last Name"
-        />
+					<Button type="submit" color="primary" size="lg">Submit</Button>
+				</Form>
+			</Formik>
 
-        <MyTextInput 
-        label="Email"
-        name="email"
-        type="email"
-        placeholder="youremail@email.com"
-        />
+			<div>
+				<p>or</p>
+			</div>
 
-        <MyTextInput
-        label= "Password"
-        name="password"
-        type="password"
-        />
-
-        <MyCheckbox name="acceptedTerms">
-            I accept the terms and conditions
-        </MyCheckbox>
-
-        <button type="submit">Submit</button>
-      </Form>
-    </Formik>
-  );
-}
-export default Register
+			<Row>
+				<p>Sign Up With</p>
+			</Row>
+		</Container>
+	);
+};
