5260c853cbd882bfba915e502d422564261b2bac
CScori <49799016+CScori@users.noreply.github.com>
2020-01-02 11:36:58 -0800

    adds rough user sidebar placeholder and style

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import './App.css';
 
 //component import for sign up and login
 import Private from "./Components/Routes/Private"
+
 // style imports
 import Header from "./Components/Header/home-header"
-
-
+import SideBar from "./Components/Header/sideBar"
 
 function App() {
   return (
    <Router>
      <div className='App'>
        <Header />
-       
+       <SideBar />
        {/* <Route exact path='/' component ={SignUp} />
        <Route exact path='/login' component ={Login} /> */}
      </div>
