744900d62f56e2c7cae8f7df8f1647b1dbfa5a92
CScori <49799016+CScori@users.noreply.github.com>
2020-01-02 21:24:27 -0800

    landing style update and link

--- src/Components/About/HowWorks.js
+++ src/Components/About/HowWorks.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button } from 'reactstrap';
+
 
 const HowWorks = () => {
     return (
@@ -7,26 +7,28 @@ const HowWorks = () => {
             <h2>How It Works</h2>
             <div className='points'>
                 <div className='point'>
-                    <div className='header'>Hipsum</div>
+                    <div className='point-header'>Hipsum</div>
                     <div className='para'>
                    lo-fi truffaut jean shorts, butcher hexagon woke thundercats brunch master cleanse chartreuse intelligentsia hashtag subway tile poutine. Flexitarian edison bulb poutine hella, authentic seitan cliche fam paleo readymade 3 wolf moon artisan food truck vegan letterpress. Post-ironic brooklyn pour-over mumblecore street art adaptogen, VHS cloud bread subway tile heirloom drinking vinegar occupy tumblr. 
                    </div>
                 </div>
                 <div className='point'>
-                    <div className='header'>Ipsum</div>
+                    <div className='point-header'>Ipsum</div>
                     <div className='para'>
                    lo-fi truffaut jean shorts, butcher hexagon woke thundercats brunch master cleanse chartreuse intelligentsia hashtag subway tile poutine. Flexitarian edison bulb poutine hella, authentic seitan cliche fam paleo readymade 3 wolf moon artisan food truck vegan letterpress. Post-ironic brooklyn pour-over mumblecore street art adaptogen, VHS cloud bread subway tile heirloom drinking vinegar occupy tumblr. 
                    </div>
                 </div>
                 <div className='point'>
-                    <div className='header'>Flannel</div>
+                    <div className='point-header'>Flannel</div>
                     <div className='para'>
                    lo-fi truffaut jean shorts, butcher hexagon woke thundercats brunch master cleanse chartreuse intelligentsia hashtag subway tile poutine. Flexitarian edison bulb poutine hella, authentic seitan cliche fam paleo readymade 3 wolf moon artisan food truck vegan letterpress. Post-ironic brooklyn pour-over mumblecore street art adaptogen, VHS cloud bread subway tile heirloom drinking vinegar occupy tumblr. 
                    </div>
                 </div>
             </div>
-           <div className='button'></div>
-            <Button color='primary'>Sign Up</Button>
+           <div >
+           <button href='/register'>Sign Up</button>
+           </div>
+            
         </div>
     )
 }
