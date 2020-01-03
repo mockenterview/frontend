1c452641d9d9f01344198a4db038dcf70cdda3f4
CScori <49799016+CScori@users.noreply.github.com>
2020-01-03 13:29:57 -0800

    adds dev team photos

--- src/Components/About/Team.js
+++ src/Components/About/Team.js
@@ -1,4 +1,11 @@
 import React from 'react'
+import Andrew from '../../images/TEAM/andrew.jpg'
+import Brian from '../../images/TEAM/brian.PNG'
+import Cori from '../../images/TEAM/Cori.PNG'
+import Danny from '../../images/TEAM/danny.PNG'
+import Derek from '../../images/TEAM/Derek.PNG'
+import Vicki from '../../images/TEAM/Vicki.PNG'
+
 const Team = () => {
     return (
         <>
@@ -7,32 +14,44 @@ const Team = () => {
          </div>
         <div className='Team'>
             <div className='Person'>
-            <div className='ig'></div>
+            <div className='ig'>
+                <img src={Andrew} alt='Andrew Ogle' className='shape'/>
+            </div>
                 <p>Andrew Ogle</p>
                 <p>Project Lead</p>
             </div>
             <div className='Person'>
-            <div className='ig'></div>
+            <div className='ig'>
+            <img src={Brian} alt='Brian Taveras' className='shape'/>
+            </div>
                 <p>Brian Taveras</p>
                 <p>Developer</p>
             </div>
             <div className='Person'>
-                <div className='ig'></div>
+                <div className='ig'>
+                <img src={Cori} alt='Cori Sternberg' className='shape'/>
+                </div>
                 <p>Cori Sternberg</p>
                 <p>Developer</p>
             </div>
             <div className='Person'>
-            <div className='ig'></div>
+            <div className='ig'>
+            <img src={Danny} alt='Danniel Vidal' className='shape'/>
+            </div>
                 <p>Danniel Vidal</p>
                 <p>Developer</p>
             </div>
             <div className='Person'>
-            <div className='ig'></div>
+            <div className='ig'>
+            <img src={Derek} alt='Derek Etman' className='shape'/>
+            </div>
                 <p>Derek Etman</p>
                 <p>Developer</p>
             </div>
             <div className='Person'>
-            <div className='ig'></div>
+            <div className='ig'>
+            <img src={Vicki} alt='Vicki Nelson' className='shape'/>
+            </div>
                 <p>Vicki Nelson</p>
                 <p>UX Designer</p>
             </div>
